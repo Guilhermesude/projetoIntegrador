2b0595cba1f845cadbe99f7fe762859f6d68b3cc
Guilherme <46380076+Guilhermesude@users.noreply.github.com>
2021-11-28 21:29:58 -0300

    conclusão

--- site/consulta.php
+++ site/consulta.php
@@ -13,10 +13,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <h1>Isso aqui funcionou!!!!!!</h1>
+    
+        
 </head>
 <body>
+    <img src="imagens/pedreiro.png" alt="some text" width=1000 height=500>
+    <h1 style="margin-left: 45px; color:black"> 
+      Página em construção!
+    </h1>
     
 </body>
 </html>
--- site/login.php
+++ site/login.php
@@ -1,17 +1,20 @@
-<form name="login" id="login" action="envioDados.php">
+<div style="
+width: 100%;
+height: 400px; margin-top: 100px">
+    <form name="login" id="login" action="envioDados.php">
 
-    <label from="email">Email</label>
-    <input name="email" type="email" id="email">
-    
-    <br><br>
+        <label from="email">Email</label>
+        <input name="email" type="email" id="email">
+        
+        <br><br>
 
-    <label from="senha">Senha</label>
-    <input name="senha" type="password" id="senha">
-    
-    <br><br>
-    <button type="button" id="dadosLogin" style="color: green;" >Entrar</button>
+        <label from="senha">Senha</label>
+        <input name="senha" type="password" id="senha">
+        
+        <br><br>
+        <button type="button" id="dadosLogin" style="color: green;" >Entrar</button>
 
-</form>
-
+    </form>
+</div>
 
 <script type="text/javascript" src="js/funcionalidadeBotoes.js" defer></script>
